bc998e1b67668b897da03bc46c76657209579473
pranav-cholleti <119649614+pranav-cholleti@users.noreply.github.com>
2026-02-01 00:32:22 +0530

    Update gemini.ts

--- services/gemini.ts
+++ services/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI, Type, FunctionDeclaration } from "@google/genai";
 
 const getClient = () => {
-  const apiKey = 'AIzaSyDBtBHCupctymdgbJQQ1N5ll1mlnDIX7_w' || process.env.GEMINI_API_KEY;
+  const apiKey =  process.env.GEMINI_API_KEY;
   if (!apiKey) throw new Error("API Key not found in environment. Please ensure GEMINI_API_KEY is set in .env.local.");
   return new GoogleGenAI({ apiKey });
 };
